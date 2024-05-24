5500e8803e5fe34a32d79f79a02c582e042f1fd7
Jérôme Fontaine <j.fontaine@infrabat-ingenierie.fr>
2024-05-25 00:54:50 +0400

    mise a jour

--- pyEurocode2/coreDCpoutre.py
+++ pyEurocode2/coreDCpoutre.py
@@ -13,53 +13,12 @@ class DCPoutreRectangulaire:
 
     def d(self):
         return self.h - self.c
-<<<<<<< HEAD
     
     def Asmin1(self):
-=======
-
-    def Asmin(self):
->>>>>>> 0b548fdc407b544de97b1c1fbcfaaf142550478f
         fctm = self.beton.fctm()
         fyk = self.acier.fyk()
         bw = self.bw
         d = self.d()
-<<<<<<< HEAD
-        return 0.26 * fctm / fyk * bw * d  
-    
-    def Asmin2(self):
-        bw = self.bw
-        d = self.d()
-        return 0.0013 * bw * d
-    
-    def Asmin(self):
-        Asmin1 = self.Asmin1()
-        Asmin2 = self.Asmin2()
-        return max(Asmin1, Asmin2)
-        
-    def Asmax(self):
-        return 0.04 * self.Ac()
-    
-    def resultat_long(self):
-        print("Béton")
-        printligne("Classe de résistance", "-", "-", f"{self.beton.classeresistance}")
-        printligne("Résitance car. compression", "fck", "MPa", f"{self.beton.fck():.2f}")
-        printligne("Résitance car. compression", "fcm", "MPa", f"{self.beton.fcm():.2f}")
-        printligne("Résitance car. traction", "fctm", "MPa", f"{self.beton.fctm():.2f}")
-        print("")
-        printsep()
-        print("Acier")
-        printligne("Limite d'élasticité", "fyk", "MPa", f"{self.acier.fyk():.2f}")
-        print("")
-        printsep()
-        print("Sections minimale et maximale d'armatures")
-        printligne("Asmin 1", "Asmin1", "cm2", f"{self.Asmin1()*1e4:.2f}")
-        printligne("Asmin 2", "Asmin2", "cm2", f"{self.Asmin2()*1e4:.2f}")
-        print("")
-        printligne("Asmin", "Asmin", "cm2", f"{self.Asmin()*1e4:.2f}")
-        printligne("Asmax", "Asmax", "cm2", f"{self.Asmax()*1e4:.2f}")
-        printfintab() 
-=======
         Asmin1 = 0.26 * fctm / fyk * bw * d
         Asmin2 = 0.0013 * bw * d
         Asmin = max(Asmin1, Asmin2)
@@ -67,4 +26,3 @@ class DCPoutreRectangulaire:
 
     def Asmax(self):
         return 0.04 * self.Ac()
->>>>>>> 0b548fdc407b544de97b1c1fbcfaaf142550478f
